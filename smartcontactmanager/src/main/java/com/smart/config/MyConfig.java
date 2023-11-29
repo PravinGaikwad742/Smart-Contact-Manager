fe30505e7ddf7703c674ef3bc1d8e2531894600d
user <user@LAPTOP-MAJ2VRFN>
2023-11-29 09:47:49 +0530

    First Commit

---
+++ smartcontactmanager/src/main/java/com/smart/config/MyConfig.java
@@ -0,0 +1,109 @@
+package com.smart.config;
+
+
+import org.springframework.context.annotation.Bean;   
+import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+//import org.springframework.security.web.DefaultSecurityFilterChain;
+//import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class MyConfig extends WebSecurityConfigurerAdapter  {
+	@Bean
+	public UserDetailsService  getUserDetailsService()
+	{
+		return new UserDetailsServiceImpl();
+	}
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder()
+	{
+		return new BCryptPasswordEncoder();
+	}
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider daoauthenticationProvider=new DaoAuthenticationProvider();
+		daoauthenticationProvider.setUserDetailsService(this.getUserDetailsService());
+		daoauthenticationProvider.setPasswordEncoder(passwordEncoder());
+		return daoauthenticationProvider;
+	}
+	
+	///Configuration Method
+	
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	     auth.authenticationProvider(authenticationProvider());
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/admin/**")
+			 .hasRole("ADMIN")
+		     .antMatchers("/user/**")
+		     .hasRole("USER")
+		     .antMatchers("/**")
+		     .permitAll()
+		     .and()
+		     .formLogin()
+      	     .loginPage("/signin")
+      	     .loginProcessingUrl("/dologin")
+      	     .defaultSuccessUrl("/user/index")
+      	   //  .failureUrl("/login-fail")
+		     .and()
+		     .csrf()
+		     .disable(); 
+	}
+	
+	
+	
+//	@Bean
+//	public DaoAuthenticationProvider authenticationProvider() {
+//		DaoAuthenticationProvider daoauthenticationProvider=new DaoAuthenticationProvider();
+//		daoauthenticationProvider.setUserDetailsService(this.getUserDetailsService());
+//		daoauthenticationProvider.setPasswordEncoder(passwordEncoder() );
+//		return daoauthenticationProvider; 
+//	}
+//	
+//	@Bean
+//	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception
+//	{
+//		return configuration.getAuthenticationManager();
+//	}
+//
+//	     
+//	@Bean
+//	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+//	{
+//        http
+//                .authorizeHttpRequests((authorize) -> authorize
+//                        .antMatchers("/admin/**")
+//                        .hasRole("ADMIN")
+//                        .antMatchers("/user/**")
+//                        .hasRole("USER")
+//                        .antMatchers("/**")
+//                        .permitAll().anyRequest().authenticated())
+//                        
+//                .formLogin(login -> login
+//                        .loginPage("/signin")
+//                        .loginProcessingUrl("/dologin")
+//                        .defaultSuccessUrl("/user/index"));
+//	 
+//		http.authenticationProvider(authenticationProvider());
+//		DefaultSecurityFilterChain defaultSecurityFilterChain = http.build();
+//		return defaultSecurityFilterChain;
+//		  
+//	}  
+//
+//	
+}
---
+++ smartcontactmanager/src/main/java/com/smart/controller/MyController.java
@@ -0,0 +1,118 @@
+package com.smart.controller;
+
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;      
+
+import org.springframework.beans.factory.annotation.Autowired ;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+import com.smart.dao.UserRepository;
+import com.smart.entities.User;
+import com.smart.helper.Message;
+
+
+ 
+
+
+
+@Controller
+public class MyController {
+	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	@Autowired
+	    private UserRepository userRepository;
+		@GetMapping("/")   
+		public String home(Model model)
+		{
+			model.addAttribute("title","This is home");
+			return "home";
+		}
+
+
+		@GetMapping("/about")   
+		public String about(Model model)
+		{
+		model.addAttribute("title","This is about");
+		return "about";
+		} 
+		
+          //Register User 
+		
+		@GetMapping("/signup")   
+		public String signup(Model model)
+		{
+		model.addAttribute("title","This is signup Page");
+		model.addAttribute("user",new User());
+		return "signup";
+		} 
+	//registering user
+		@PostMapping("/do_register")
+		public String registerUser(@Valid @ModelAttribute("user") User user,BindingResult result,@RequestParam(value = "agreement",defaultValue = "false")boolean agreement,
+				                                   Model model,HttpSession session)
+		{ 
+			
+			try {
+				if(!agreement)
+				{
+					System.out.println("You have not agreed the term conditions");
+					throw new Exception("You have not agreed the term conditions");
+				}
+				if(result.hasErrors())
+				{   
+					System.out.println(result.hasErrors());
+					System.out.println("Error"+result.toString());
+					model.addAttribute("user",user);
+					return "signup";
+				}
+				System.out.println(user);
+				System.out.println(agreement);
+				 user.setRole("ROLE_USER");
+				user.setEnabled(true);
+				user.setImageUrl("membership.png");
+				user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+			
+				User save = this.userRepository.save(user);
+				System.out.println(save);
+				model.addAttribute("user",new User());
+				session.setAttribute("message",new Message("Successfully register!","alert-Success"));
+//				if(session.isNew())
+//				{
+//					session.removeAttribute("message");
+//				}
+				return "signup"; 
+				
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+			
+				e.printStackTrace();
+				model.addAttribute("user",user);
+				session.setAttribute("message",new Message("Something Went Wrong !!"+e.getMessage(),"alert-error"));
+				session.setAttribute("emailmsg",new Message("must be a well-formed email address!!"+e.getMessage(),"alert-error"));
+			
+//				model.addAttribute("shd",);
+				
+				
+				return "signup";
+			}
+			
+		}
+
+       //Handler for Custome Login
+		@GetMapping("/signin")
+		public String login(Model model)
+		{
+			model.addAttribute("title","Login Page");
+			return "login";
+		}
+}
